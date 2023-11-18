10f55d687e60189e69a461e6f64e9384e3bae900
andrewdupere <andrewdupere@outlook.com>
2023-11-18 17:57:58 -0500

    update path

--- changer.py
+++ changer.py
@@ -3,12 +3,20 @@
 
 
 # Function to replace a specific line in an HTML file
+from datetime import datetime
+
+
+html_file_path = r"/var/www/html/index2.html"
+line_number = 19
+new_content = 'last update' + str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+
 def replace_line_in_html(html_file_path, line_number, new_content):
     with open(html_file_path, 'r') as file:
         lines = file.readlines()
 
     if 0 < line_number <= len(lines):
-        lines[line_number - 1] = new_content + '\n'
+        lines[line_number - 1] = new_content
 
         with open(html_file_path, 'w') as file:
             file.writelines(lines)
@@ -16,14 +24,6 @@ def replace_line_in_html(html_file_path, line_number, new_content):
     else:
         print(f"Invalid line number: {line_number}")
 
-# Specify the path to your HTML file
-html_file_path = 'path/to/your/file.html'
 
-# Specify the line number you want to replace
-line_number_to_replace = 5
+replace_line_in_html(html_file_path, line_number, new_content)
 
-# Specify the new content to replace the line
-new_content = '<p>New content for the line</p>'
-
-# Replace the specified line in the HTML file
-replace_line_in_html(html_file_path, line_number_to_replace, new_content)
